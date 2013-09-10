d9884cefbb74d0eb8ca05e1912ca0e785396a977
haist2002 <haist2002@gmail.com>
2013-09-11 00:22:29 +0900

    mybatis 연동중 mapper 에러

--- src/main/java/akhouse/spring/basic/home/HomeDaoImpl.java
+++ src/main/java/akhouse/spring/basic/home/HomeDaoImpl.java
@@ -2,20 +2,19 @@ package akhouse.spring.basic.home;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class HomeDaoImpl implements HomeDao {
+public class HomeDaoImpl extends SqlSessionDaoSupport  implements HomeDao {
 
-	
-	@Autowired
-	private  SqlSession sqlSession;
+
 	
 	@Override
 	public List<boardSpec> selectBoardList() {
-		return sqlSession.selectList("sqlBasicMapper.select-board-list");
+		
+		
+		return getSqlSession().selectList("sqlBasicMapper.select-board-list");
 	}
 	
 }
